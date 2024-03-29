5228a52b8a6d9023ce45981498d20039d300c466
AntonShiva <ryashinanthon@yandex.ru>
2024-03-29 12:38:47 +0300

    Initial Commit

---
+++ Pranayama and meditation/Pranayama_and_meditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pranayama_and_meditationApp.swift
+//  Pranayama and meditation
+//
+//  Created by Anton Rasen on 29.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Pranayama_and_meditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
